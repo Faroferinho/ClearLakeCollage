87626f83c6c73704f32f8af408179eb6921e5afd
Davi <gonca.davi@gmail.com>
2025-04-06 16:10:51 -0300

    conserto

--- Back-End/src/main/java/org/example/repositories/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repositories;
-
-import org.example.models.Livro;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LivroRepository extends MongoRepository<Livro, String> {
-}
