270d21c45229721badd3b6d3eff6de5fec69cf2d
Faroferinho <conrado.perini.fracacio@gmail.com>
2025-04-09 19:09:17 -0300

    Start of the design changes

--- clearlakecollege/src/App.js
+++ clearlakecollege/src/App.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 const API_URL = "http://localhost:8081/projeto/api/v1/aluno";
 
-export default function(){
+export default function StudentsCRUD(){
   const [students, setStudents] = useState([]);
   const [student, setStudent] = useState({nome: "", telefone: "", email: "", endereco: ""});
   const [edition, setEdition] = useState(null);
@@ -45,21 +45,23 @@ export default function(){
   };
 
   return (
-    <div>
-      <div class="form">
-        <h1>Inserção Alunos</h1>
-        <div>
+    <div className='max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg'>
+      <div>
+        <h1 className="text-2xl font-bold mb-4 text-center">Cadastro</h1>
+        <sub>Insira o nome de Novos Alunos</sub>
+        <div class="form">
           <input type='text' placeholder='nome' value={student.nome} onChange={(e) => setStudent({...student, nome: e.target.value})}/><br/>
           <input type='text' placeholder='telefone' value={student.telefone} onChange={(e) => setStudent({...student, telefone: e.target.value})}/><br/>
           <input type='text' placeholder='email' value={student.email} onChange={(e) => setStudent({...student, email: e.target.value})}/><br/>
           <input type='text' placeholder='endereço' value={student.endereco} onChange={(e) => setStudent({...student, endereco: e.target.value})}/><br/>
+          <button onClick={handleSubmit}>
+            {edition ? "Atualizar" : "Adicionar"}
+          </button>
         </div>
-        <button onClick={handleSubmit}>
-          {edition ? "Atualizar" : "Adicionar"}
-        </button>
       </div>
 
       <div class="table">
+        <h1>Lista de Alunos</h1>
         <thead>
           <tr>
             <th>Nome</th>
